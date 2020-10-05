42960a3e297cf01d4475663c886b8dbbd5d1e94a
Oleh <oleg_ai@icloud.com>
2020-10-05 14:34:21 +0300

    Fixed deduction

--- main.js
+++ main.js
@@ -22,93 +22,105 @@ function taxes() {
     const incomeCeil4  = 163300;
     const incomeCeil5  = 207350;
     const incomeFloor6 = 518400;
-
+    
+    const deduction =  12400;
 
     let income = parseFloat(document.getElementById("income").value);    //Getting the value
     console.log(income);
+    var outDeduction = income - deduction;
 
-    if (income <= incomeCeil1){                  
+    if (income <= deduction) {
 
-        let tax = income * feeSingle1;
+        let result = document.getElementById("output");
+        result.innerHTML = "You are so poor, too pay a tax ((";
+
+    } else if (outDeduction <= incomeCeil1){                  
+
+        let tax = outDeduction * feeSingle1;
         console.log(tax);
         let result = document.getElementById("output");
         result.innerHTML = tax;
 
 
-    } else if ((income > incomeCeil1) && (income <= incomeCeil2)) {
+    } else if ((outDeduction > incomeCeil1) && (income <= incomeCeil2)) {
         
-        let previousTax = incomeCeil1 * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
         console.log(tax);
         let result = document.getElementById("output");
         result.innerHTML = tax;
 
-    } else if ((income > incomeCeil2) && (income <= incomeCeil3)) {
+    } else if ((outDeduction > incomeCeil2) && (income <= incomeCeil3)) {
 
-        let previousTax = incomeCeil1 * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
-        let difference1 = income - incomeCeil2;
+        let difference1 = outDeduction - incomeCeil2;
         let change1 = difference1 * feeSingle3;
         let tax1 = tax + change1;
         console.log(tax1);
         let result = document.getElementById("output");
         result.innerHTML = tax1;
         
-    } else if ((income > incomeCeil3) &&  (income <= incomeCeil4)) {
+    } else if ((outDeduction > incomeCeil3) &&  (income <= incomeCeil4)) {
 
-        let previousTax = incomeCeil1 * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
-        let difference1 = income - incomeCeil2;
+        let difference1 = outDeduction - incomeCeil2;
         let change1 = difference1 * feeSingle3;
         let tax1 = tax + change1;
-        let difference2 = income - incomeCeil3;
+        let difference2 = outDeduction - incomeCeil3;
         let change2 = difference2 * feeSingle4;
         let tax2 = tax1 + change2;
         console.log(tax2);
         let result = document.getElementById("output");
         result.innerHTML = tax2;
 
-    } else if ((income > incomeCeil4) && (income <= incomeCeil5)) {
+    } else if ((outDeduction > incomeCeil4) && (income <= incomeCeil5)) {
 
-        let previousTax = incomeCeil1 * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
-        let difference1 = income - incomeCeil2;
+        let difference1 = outDeduction - incomeCeil2;
         let change1 = difference1 * feeSingle3;
         let tax1 = tax + change1;
-        let difference2 = income - incomeCeil3;
+        let difference2 = outDeduction - incomeCeil3;
         let change2 = difference2 * feeSingle4;
         let tax2 = tax1 + change2;
-        let difference3 = income - incomeCeil4;
+        let difference3 = outDeduction - incomeCeil4;
         let change3 = difference3 * feeSingle5;
         let tax3 = tax2 + change3;
         console.log(tax3);
         let result = document.getElementById("output");
         result.innerHTML = tax3;
 
-    } else if ((income > incomeCeil5) && (income <= incomeFloor6)) {
+    } else if ((outDeduction > incomeCeil5) && (income <= incomeFloor6)) {
 
-        let previousTax = incomeCeil1 * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
-        let difference1 = income - incomeCeil2;
+        let difference1 = outDeduction - incomeCeil2;
         let change1 = difference1 * feeSingle3;
         let tax1 = tax + change1;
-        let difference2 = income - incomeCeil3;
+        let difference2 = outDeduction - incomeCeil3;
         let change2 = difference2 * feeSingle4;
         let tax2 = tax1 + change2;
-        let difference3 = income - incomeCeil4;
+        let difference3 = outDeduction - incomeCeil4;
         let change3 = difference3 * feeSingle5;
         let tax3 = tax2 + change3;
-        let difference4 = income - incomeCeil5;
+        let difference4 = outDeduction - incomeCeil5;
         let change4 = difference4 * feeSingle6;
         let tax4 = tax3 + change4;
         console.log(tax4);
@@ -117,23 +129,24 @@ function taxes() {
 
     } else {
 
-        let previousTax = income * feeSingle1;
-        let difference = income - incomeCeil1;
+        
+        let previousTax = outDeduction * feeSingle1;
+        let difference = outDeduction - incomeCeil1;
         let change = difference * feeSingle2;
         let tax = previousTax + change;
-        let difference1 = income - incomeCeil2;
+        let difference1 = outDeduction - incomeCeil2;
         let change1 = difference1 * feeSingle3;
         let tax1 = tax + change1;
-        let difference2 = income - incomeCeil3;
+        let difference2 = outDeduction - incomeCeil3;
         let change2 = difference2 * feeSingle4;
         let tax2 = tax1 + change2;
-        let difference3 = income - incomeCeil4;
+        let difference3 = outDeduction - incomeCeil4;
         let change3 = difference3 * feeSingle5;
         let tax3 = tax2 + change3;
-        let difference4 = income - incomeCeil5;
+        let difference4 = outDeduction - incomeCeil5;
         let change4 = difference4 * feeSingle6;
         let tax4 = tax3 + change4;
-        let difference5 = income - incomeFloor6;
+        let difference5 = outDeduction - incomeFloor6;
         let change5 = difference5 * feeSingle7;
         let tax5 = tax4 + change5;
         console.log(tax5);
